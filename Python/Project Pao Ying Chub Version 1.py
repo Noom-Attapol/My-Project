1e7b325fff91e4ad2953b2d450818a714890fc6b
Noom-Attapol <114787927+Noom-Attapol@users.noreply.github.com>
2022-10-22 16:51:28 +0700

    Update Project Pao Ying Chub Version 1.py

--- Python/Project Pao Ying Chub Version 1.py
+++ Python/Project Pao Ying Chub Version 1.py
@@ -1,34 +1,52 @@
-import random
-user_pao=''
-random_pao=''
-result=''
-win=0
-loss=0
-tile=0
-print("Welcome to PAO YING CHUB")
-while True:
-    user_pao = input("Enter your choice (hammer, scissors, paper or exit) : ")
-    if user_pao == "exit":
-        break
-    if user_pao == "hammer" or user_pao == "scissors" or user_pao == "paper":
-        random_pao = random.choice(["hammer","scissors","paper"])
-        if user_pao == random_pao:
-            result = "tile"
-            tile = tile + 1
-        elif user_pao == "hammer" and random_pao == "paper":
-            result = "lost"
-            lost = lost + 1
-        elif user_pao == "scissors" and random_pao == "hammer":
-            result = "lost"
-            lost = lost + 1
-        elif user_pao == "paper" and random_pao == "scissors":
-            result = "loss"
-            loss = loss + 1
-        else :
-            result = "win"
-            win = win + 1
-        print(f"Your Choice is {user_pao} and Computer is {random_pao}")
-        print(f"Result : {result}")
-        print(f"Win :{win}  Tile : {tile} Loss : {loss}")
-    else:
-        print("Invalid Typing Please Try again")
+## Pao Ying Chub 1
+pyc1 <- function() {
+  pyc_choices <- c("rock", "scissors", "paper")
+  your_wins <- 0
+  your_ties <- 0
+  your_losses <- 0
+
+    while (TRUE) { 
+    my_pyc <- readline("Your Choice:rock,scissors,paper,quit: ")
+
+      if ( my_pyc == "quit" ) {
+          message(my_pyc) 
+          break
+      } else {
+          com_pyc <- sample(pyc_choices,1)
+          message(paste(my_pyc,":",com_pyc))
+          if (my_pyc == "rock" & com_pyc == "rock") {
+              message("you tie")
+              your_ties <- your_ties+1
+            } else if (my_pyc == "rock" & com_pyc == "scissors") {
+              message("you win")
+              your_wins <- your_wins+1
+            } else if (my_pyc == "rock" & com_pyc == "paper") {
+              message("you lose")
+              your_losses <- your_losses+1
+            } else if (my_pyc == "scissors" & com_pyc == "rock") {
+              message("you lose")
+              your_losses <- your_losses+1
+            } else if (my_pyc == "scissors" & com_pyc == "scissors") {
+              message("you tie")
+              your_ties <- your_ties+1
+            } else if (my_pyc == "scissors" & com_pyc == "paper") {
+              message("you win")
+              your_wins <- your_wins+1
+            } else if (my_pyc == "paper" & com_pyc == "rock") {
+              message("you win")
+              your_wins <- your_wins+1
+            } else if (my_pyc == "paper" & com_pyc == "scissors") {
+              message("you lose")
+              your_losses <- your_losses+1
+            } else if (my_pyc == "paper" & com_pyc == "paper") {
+              message("you tie")
+              your_ties <- your_ties+1
+          } else {
+              message("You enter wrong condition")
+##              result_pyc <- glue("{my_pyc}:{com_pyc}")
+##              print(result_pyc)
+          }
+          message(paste("wins=",your_wins,",ties=",your_ties,",losses=",your_losses))
+      }
+    }
+}
